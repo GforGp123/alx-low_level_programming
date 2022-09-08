2aef0013754926b1a39cd2bbcc86484c3839043c
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-08 16:52:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
  #include <stdio.h>
-/* Print quoted, add new line then return */
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+/**
+ * main - Print "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
