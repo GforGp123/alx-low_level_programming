469fea2ac62db89f833ebc6b23392f53c32d9c2a
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-08 19:49:54 -0700

    6-size.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stderr.h>
+/**
+ * main - Entry point
+ * Return 1 as success
+ * Description - print quoted
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
