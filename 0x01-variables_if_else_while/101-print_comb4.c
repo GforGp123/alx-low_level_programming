c65380b5290501e268742202e76433bedc1f231f
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-10 15:25:20 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,13 +5,15 @@
  */
 int main(void)
 {
-	int a, b, c;
+	int a;
+	int b;
+	int c;
 
-	for (a = '0'; a < 10; a++)
+	for (a = '0'; a < '9'; a++)
 	{
-	for (b = a + 1; b <= 9; b++)
+	for (b = a + 1; b <= '9'; b++)
 	{
-	for (c = b + 1; c <= 9; c++)
+	for (c = b + 1; c <= '9'; c++)
 	{
 	putchar(a);
 	putchar(b);
