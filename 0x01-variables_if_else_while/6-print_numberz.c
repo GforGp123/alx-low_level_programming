62723e675363c67334dbbd7ef4daadf23c933673
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-10 03:56:23 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int a = 0;
+	while (a < 10)
+	{
+		putchar(a % 10 + '0');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
