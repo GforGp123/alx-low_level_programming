f148a41de5a08a35a7c110b264facd2b0a52dd66
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-14 09:51:31 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function adds two integers and returns result
+ * @x: an integer
+ * @y: another integer
+ * Return: result
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
