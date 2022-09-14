b1bbc01c7ad17a0bf50eb388194f72e370a14704
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-14 03:34:24 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - function that prints a sign
+ * @n: prints result assigned
+ * Return: returns values of n
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
