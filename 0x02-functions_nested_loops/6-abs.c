10867a33a09db3b5cfebaf3dbc727d3a66f0499c
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-14 04:00:28 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - function that computes absolute value of an integer
+ * @n: integer
+ * Returns: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (abs(n));
+	}
+	else if (n == 0)
+	{
+	}
+}
