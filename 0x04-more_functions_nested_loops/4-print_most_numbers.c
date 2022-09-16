be9d307e5e73f787663bad5305f071f23795538f
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-15 17:56:00 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@
 
 void print_most_numbers(void)
 {
-	int n = 0;
+	int n;
 
-	for (; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		if (n != 2 && n != 4)
-			_putchar(n + '0');
+			_putchar(n + 48);
 	}
 	_putchar('\n');
 }
