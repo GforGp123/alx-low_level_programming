bd25e3ae865d45069a070c6105409cd7f048b0e7
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-16 15:46:27 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,7 +10,16 @@ void print_line(int n)
 {
 	int a;
 
-	for (a = 0; a < n; a++)
-		_putchar('_');
-	_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line
+ * @n: number of times the character \ is printed
+ * Return: Nothing
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; ++i)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
