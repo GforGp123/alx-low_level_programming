a84676abbc1d655efb9404c10b377bde542b081f
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-16 15:16:21 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: number of times the character _ should  be printed
+ * Return: Nothing
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
