cf89b204ad549346dd2f7f1b40a8b5625e271873
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-16 19:38:39 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: the integer
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int x, y, z;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		x = n * -1;
+	}
+	else
+	{
+		x = n;
+	}
+	y = x;
+	z = -1;
+
+	while (y > 9)
+	{
+		y /= 10;
+		z *= 10;
+	}
+	for (; z >= 1; z /= 10)
+	{
+		_putchar(((x / z) % 10) + 48);
+	}
+}
