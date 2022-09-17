9d8534aca68411b9b4bc7d590cb7015d84ae2be5
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-16 19:19:48 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor of the number
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int a, prime;
+
+	n = 612852475143;
+	for (prime = 2; prime <= a; prime++)
+	{
+		if (a % prime == 0)
+		{
+			a /= prime;
+			prime--;
+		}
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
