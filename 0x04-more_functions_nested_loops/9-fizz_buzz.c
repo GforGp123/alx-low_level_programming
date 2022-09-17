2a80fc8b6ef3e630ab8a1ee5fb300912c7227da6
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-16 18:51:50 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,21 +9,25 @@ int main(void)
 {
 	int c;
 
-	for (c = 1; a <= 100; c++)
+	for (c = 1; c <= 100; c++)
 	{
-		printf("%d ", c);
-	}
-	if (c % 3 == 0)
-	{
-		printf("Fizz", c);
-	}
-	else if (c % 5 == 0)
-	{
-		printf("Buzz", c);
-	}
-	else if (c % 3 && c % 5)
-	{
-		printf("FizzBuzz", c);
+		if ((c % 3 == 0) && (c % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", c);
+		}
 	}
 	printf("\n");
+	return (0);
 }
