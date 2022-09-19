dee93b23d420bdb3e77cdc96cce4652094d1db88
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-19 16:34:11 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: the string to print
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
