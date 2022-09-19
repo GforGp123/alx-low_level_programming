77f6f0b614ad4add51a8a9d9152f0ad4ac421ab1
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-19 15:24:26 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,8 @@
 
 /**
  * swap_int - function that swaps the values of two integers
- * @a: first integer* @b: second integer
+ * @a: first integer
+ *@b: second integer
  * Return: void
  */
 void swap_int(int *a, int *b)
