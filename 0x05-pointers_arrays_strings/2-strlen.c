7c9627169736b65f8f6b38fb4e85b307ac4ea347
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-19 15:37:13 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - functions that returns the length of a string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	return (_strlen(*s));
+}
