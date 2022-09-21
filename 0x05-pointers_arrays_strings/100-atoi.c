0477fe3ec2e5b33e94b0419e7068bb3b8f52539a
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-20 17:55:34 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string
+ * Return: integer converted from string
+ */
+int _atoi(char *s)
+{
+	int x, y, z, len, n, dig;
+
+	x = 0;
+	y = 0;
+	z = 0;
+	n = 0;
+	dig = 0;
+	for (len = 0; s[len] != '\0'; len++)
+	while (x < len && n == 0)
+	{
+		if (s[x] == '-')
+			++y;
+		if (s[x] >= '0' && s[x] <= '9')
+		{
+			dig = s[x] - '0';
+			if (y % 2)
+				dig = -dig;
+			z *= 10 + dig;
+			n = 1;
+			if (s[x + 1] < '0' || s[x + 1] > '9')
+				break;
+			n = 0;
+		}
+		x++;
+	}
+	if (n == 0)
+		return (0);
+	return (z);
+}
