2845c30284b026c12a554e14cb72bdb12b6fa6da
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-22 16:50:59 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - functions that encode a string into 1337
+ * @s: string
+ * Return: result
+ */
+char *leet(char *s)
+{
+	int tmp = 0, n;
+	int upp[] = {65, 69, 79, 84, 76};
+	int low[] = {97, 101, 111, 116, 108};
+	int num[] = {52, 51, 48, 55, 49};
+
+	while (s[tmp] != '\0')
+	{
+		for (n = 0; n < 5; n++)
+		{
+			if (c[tmp] == low[n] || s[tmp] == upp[n])
+			{
+				s[tmp] = num[n];
+				break;
+			}
+		}
+		tmp++;
+	}
+	return (s);
+}
