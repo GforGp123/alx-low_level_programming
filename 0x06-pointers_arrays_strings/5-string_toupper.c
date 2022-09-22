d219b7c6d1f9772963e925fbb4b79b2b7dece046
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-22 16:19:06 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string
+ * Return: result
+ */
+char *string_toupper(char *s)
+{
+	int upp = 0;
+
+	while (s[upp] != '\0')
+	{
+		if ((s[upp] >= 97) && (s[upp] <= 122))
+			s[upp] = s[upp] - 32;
+		upp++;
+	}
+	return (s);
+}
