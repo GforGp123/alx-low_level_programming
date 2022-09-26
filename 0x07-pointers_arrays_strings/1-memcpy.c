2ba255c3940153650bb013bbde9b76a4f6292c80
Godspower Okoduwa <godspowerokoduwa45@gmail.com>
2022-09-26 16:47:33 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
